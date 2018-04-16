99a76db9c649be35cd60d51139402ef79b11753d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/Q453145/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using DevExpress.Xpf.Grid;
+using System.Diagnostics;
+
+namespace Q453145
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            treeListControl1.ItemsSource = CreateData(10);
+        }
+
+        List<DataItem> CreateData(int length)
+        {
+            List<DataItem> list = new List<DataItem>();
+            for (int i = 0; i < length; i++)
+            {
+                list.Add(new DataItem() { ParentId = i % 2, Name = "Item " + i });
+            }
+            return list;
+        }
+
+        private void treeListControl1_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftCtrl && dropManager.DraggingRows != null && dropManager.DraggingRows.Count > 0)
+            {
+                if (e.IsUp)
+                    dropManager.ViewInfo.SetValue(CopyRowsHelper.CopyRowsProperty, false);
+                else if (!(bool)dropManager.ViewInfo.GetValue(CopyRowsHelper.CopyRowsProperty))
+                    dropManager.ViewInfo.SetValue(CopyRowsHelper.CopyRowsProperty, true);
+            }
+        }
+
+        private void dropManager_Drop(object sender, DevExpress.Xpf.Grid.DragDrop.TreeListDropEventArgs e)
+        {
+            if ((bool)e.Manager.ViewInfo.GetValue(CopyRowsHelper.CopyRowsProperty))
+                foreach (TreeListNode node in e.DraggedRows)
+                {
+                    DataItem item = node.Content as DataItem;
+                    node.Content = new DataItem() { Name = item.Name };
+                }
+        }
+    }
+
+    public class DataItem
+    {
+        static int IdSequence = 0;
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+
+        public DataItem()
+        {
+            Id = IdSequence++;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class CopyRowsHelper : DependencyObject
+    {
+        public static readonly DependencyProperty CopyRowsProperty = DependencyProperty.RegisterAttached("CopyRows", typeof(bool), typeof(CopyRowsHelper), new UIPropertyMetadata(false));
+
+        public static bool GetCopyRows(DependencyObject target)
+        {
+            return (bool)target.GetValue(CopyRowsProperty);
+        }
+        public static void SetCopyRows(DependencyObject target, bool value)
+        {
+            target.SetValue(CopyRowsProperty, value);
+        }
+    }
+}
---
+++ VB/Q453145/MainWindow.xaml.vb
@@ -0,0 +1,97 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows
+Imports System.Windows.Input
+Imports DevExpress.Xpf.Grid
+Imports System.Diagnostics
+
+Namespace Q453145
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			treeListControl1.ItemsSource = CreateData(10)
+		End Sub
+
+		Private Function CreateData(ByVal length As Integer) As List(Of DataItem)
+			Dim list As New List(Of DataItem)()
+			For i As Integer = 0 To length - 1
+				list.Add(New DataItem() With {.ParentId = i Mod 2, .Name = "Item " & i})
+			Next i
+			Return list
+		End Function
+
+		Private Sub treeListControl1_PreviewKeyDown(ByVal sender As Object, ByVal e As KeyEventArgs)
+			If e.Key = Key.LeftCtrl AndAlso dropManager.DraggingRows IsNot Nothing AndAlso dropManager.DraggingRows.Count > 0 Then
+				If e.IsUp Then
+					dropManager.ViewInfo.SetValue(CopyRowsHelper.CopyRowsProperty, False)
+				ElseIf (Not CBool(dropManager.ViewInfo.GetValue(CopyRowsHelper.CopyRowsProperty))) Then
+					dropManager.ViewInfo.SetValue(CopyRowsHelper.CopyRowsProperty, True)
+				End If
+			End If
+		End Sub
+
+		Private Sub dropManager_Drop(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.DragDrop.TreeListDropEventArgs)
+			If CBool(e.Manager.ViewInfo.GetValue(CopyRowsHelper.CopyRowsProperty)) Then
+				For Each node As TreeListNode In e.DraggedRows
+					Dim item As DataItem = TryCast(node.Content, DataItem)
+					node.Content = New DataItem() With {.Name = item.Name}
+				Next node
+			End If
+		End Sub
+	End Class
+
+	Public Class DataItem
+		Private Shared IdSequence As Integer = 0
+		Private privateId As Integer
+		Public Property Id() As Integer
+			Get
+				Return privateId
+			End Get
+			Set(ByVal value As Integer)
+				privateId = value
+			End Set
+		End Property
+		Private privateParentId As Integer
+		Public Property ParentId() As Integer
+			Get
+				Return privateParentId
+			End Get
+			Set(ByVal value As Integer)
+				privateParentId = value
+			End Set
+		End Property
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+
+		Public Sub New()
+			Id = IdSequence
+			IdSequence += 1
+		End Sub
+
+		Public Overrides Function ToString() As String
+			Return Name
+		End Function
+	End Class
+
+	Public Class CopyRowsHelper
+		Inherits DependencyObject
+		Public Shared ReadOnly CopyRowsProperty As DependencyProperty = DependencyProperty.RegisterAttached("CopyRows", GetType(Boolean), GetType(CopyRowsHelper), New UIPropertyMetadata(False))
+
+		Public Shared Function GetCopyRows(ByVal target As DependencyObject) As Boolean
+			Return CBool(target.GetValue(CopyRowsProperty))
+		End Function
+		Public Shared Sub SetCopyRows(ByVal target As DependencyObject, ByVal value As Boolean)
+			target.SetValue(CopyRowsProperty, value)
+		End Sub
+	End Class
+End Namespace
